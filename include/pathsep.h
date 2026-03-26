cd7a92aba84546219f932f58d8ccce38916f0330
otors <186635411+otors@users.noreply.github.com>
2026-03-26 03:43:20 -0300

    feat: type now accepts multiple args and search path for executables

---
+++ include/pathsep.h
@@ -0,0 +1,6 @@
+
+#ifdef _WIN32
+#define PATH_SEP ";"
+#else
+#define PATH_SEP ":"
+#endif
--- src/main.c
+++ src/main.c
@@ -1,9 +1,13 @@
+#include <dirent.h>
+#include <string.h>
+#include "../include/pathsep.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
+  const char *env_path = getenv("PATH");
   while (1)
   {
     // Flush after every printf
@@ -27,17 +31,58 @@ int main(int argc, char *argv[])
 
     if (!strncmp(input, "type ", 5))
     {
-      char arg[100];
-      strcpy(arg, input + 5);
+      char *arg = strdup(input + 5);
+      char *arg_it, *dir_it;
+      char *arg_tk = strtok_r(arg, " ", &arg_it);
 
-      if (!strcmp(arg, "exit") || !strcmp(arg, "echo") || !strcmp(arg, "type"))
+      while (arg_tk != NULL)
       {
-        printf("%s is a shell builtin\n", arg);
+        if (!strcmp(arg_tk, "exit") || !strcmp(arg_tk, "echo") || !strcmp(arg_tk, "type"))
+        {
+          printf("%s is a shell builtin\n", arg_tk);
+          arg_tk = strtok_r(NULL, " ", &arg_it);
+          continue;
+        }
+
+        char *path_cp = strdup(env_path);
+        char *dir_tk = strtok_r(path_cp, PATH_SEP, &dir_it);
+        char found = 0;
+        while (dir_tk != NULL)
+        {
+          DIR *dir;
+          if ((dir = opendir(dir_tk)) == NULL)
+          {
+            dir_tk = strtok_r(NULL, PATH_SEP, &dir_it);
+            continue;
+          }
+          struct dirent *entry;
+          while ((entry = readdir(dir)) != NULL)
+          {
+            if (!strcmp(entry->d_name, arg_tk))
+            {
+              char full_path[1024];
+              snprintf(full_path, sizeof(full_path), "%s/%s", dir_tk, entry->d_name);
+              if (!access(full_path, X_OK))
+              {
+                printf("%s is %s\n", arg_tk, full_path);
+                found = 1;
+                break;
+              }
+            }
+          }
+          closedir(dir);
+          if (found)
+            break;
+          dir_tk = strtok_r(NULL, PATH_SEP, &dir_it);
+        }
+        if (!found)
+        {
+          printf("%s: not found\n", arg_tk);
+        }
+        free(path_cp);
+        arg_tk = strtok_r(NULL, " ", &arg_it);
       }
-      else
-      {
-        printf("%s: not found\n", arg);
-      }
+      free(arg);
       continue;
     }
 
